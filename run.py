fdbfbcc38c641bc0d8aab58302fcfb7d87d941e4
0707yiliu <0707yi.liu@gmail.com>
2024-11-18 21:47:49 +0000

    reset issue

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import gymnasium as gym
+import numpy as np
+
 import gymnasium_envs
 from stable_baselines3 import PPO
 
 env = gym.make("Chef-v0")
 env.reset()
+while 1:
+    env.step(np.zeros(14))
