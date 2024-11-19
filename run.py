4e7d3e7017139ef96c84209ce7acb1d49361a913
0707yiliu <0707yi.liu@gmail.com>
2024-11-19 22:10:25 +0000

    yaml config

--- run.py
+++ run.py
@@ -2,11 +2,17 @@ import time
 
 import gymnasium as gym
 import numpy as np
-
 import gymnasium_envs
 from stable_baselines3 import PPO
+import yaml
 
-env = gym.make("Chef-v0")
+with open('config/chef_v0.yml', 'r', encoding='utf-8') as cfg:
+    config = yaml.load(cfg, Loader=yaml.FullLoader)
+
+
+env = gym.make(
+    config['task_name']
+)
 env.reset()
 while True:
     obs, reward, terminated, _, info = env.step(np.zeros(14))
